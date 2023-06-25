ac5e004d3334d484063689803545cc9665b5c707
AmirAlakbarli <emirelekberli@gmail.com>
2023-06-25 23:29:56 +0400

    update: problem 3 and problem 4

--- Problem3/Problem3.java
+++ Problem3/Problem3.java
@@ -1,29 +1,6 @@
 import java.util.*;
 
 public class Problem3 {
-
-    public static boolean areDigitsUnique(int number) {
-        String numberString = String.valueOf(number);
-        int length = numberString.length();
-
-        for (int i = 0; i < length - 1; i++) {
-            char digit = numberString.charAt(i);
-
-            for (int j = i + 1; j < length; j++) {
-                if (digit == numberString.charAt(j)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    public static boolean has5Digits(int number) {
-        String numberString = String.valueOf(number);
-        return numberString.length() == 5;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
@@ -32,12 +9,15 @@ public class Problem3 {
 
         List<Integer> numbersWithDifferentDigits = new ArrayList<>();
         for (int i = a; i <= b; i++) {
-            if (has5Digits(i) && areDigitsUnique(i))
+            int decimals = i % 10;
+            int hundredths = (i / 10) % 10;
+            int thousandths = (i / 100);
+            if (decimals != hundredths && decimals != thousandths && hundredths != thousandths) {
                 numbersWithDifferentDigits.add(i);
+            }
         }
-
-        for (int number : numbersWithDifferentDigits)
-            System.out.println(number);
+        
+        for(int number: numbersWithDifferentDigits) System.out.println(number);
 
     }
 }
